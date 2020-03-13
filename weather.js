fa2f99efff7c8243b6efccd7a8cc85ee8e78610d
Armando Velazquez <davidarmando.gdl@live.com>
2020-03-12 20:18:58 -0600

    add c° units

--- weather.js
+++ weather.js
@@ -3,7 +3,7 @@ const API_KEY = "2915ce5b8d2735204f8a2d49c9510b33";
 const getWeather = async () => {
   const city = `${document.getElementById("cityInput").value}`;
   const data = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
   const parsedData = await data.json();
   console.log(parsedData);
